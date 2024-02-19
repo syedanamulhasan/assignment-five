67325d3446eb9871efef0546b6b3cdb1e24cf9a4
syedanamulhasan <syedanamulhasan@gmail.com>
2024-02-19 14:22:40 +0600

    click js script added

--- ticket.js
+++ ticket.js
@@ -1,12 +1,32 @@
 const allBtn= document.getElementsByClassName('btn-ghost');
+
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function onClick() {
+//   btn.style.backgroundColor = 'salmon';
+//   btn.style.color = 'white';
+// });
+// document.getElementsByClassName('btn-ghost').onmouseover = function() {
+//     this.style.color = 'red';
+//   };
+// const btnIndex= allBtn[index];
+
+// for(let index=0; index<allBtn.length;index++){
+//     const button= allBtn[index];
+//     // console.log(element);
+//     button.addEventListener('click',function(){
+//         console.log('clicked');
+//     })
+// }
+
 let seats=40;
 let count=0;
 for (const btn of allBtn){
     btn.addEventListener('click',function(e){
         seats=seats-1;
         count=count+1;
-
-
+        // const btn=e.target;
+        // console.log(btn);
         const seatName=e.target.parentNode.childNodes[3].innerText;
         const ticketReceipt=document.getElementById('ticket-receipt');
         const li=document.createElement('li');
@@ -16,6 +36,8 @@ for (const btn of allBtn){
         p.innerText= 'Economy';
         const p2=document.createElement('p');
 
+        e.target.style.backgroundColor='lime';
+        e.target.setAttribute("disabled", false);;
         p2.innerText=550;
         li.appendChild(p1);
         li.appendChild(p);
@@ -29,7 +51,12 @@ for (const btn of allBtn){
         const grandTotal= document.getElementById('grand-total').innerText
         const convertedGrandTotal=parseInt(grandTotal);
         const sum2 = convertedGrandTotal + parseInt (p2.innerText);
-
+        if(sum2>2200){
+            alert("you can't select more than 4 tickets")
+            return;
+        }
+        // totalPrice('total-price',parseInt(p2.innerText))
+        // grandTotalCost('grand-total'parseInt(p2.innerText))
         setInnerText('grand-total', sum2);
         setInnerText('total-price', sum);
         setInnerText("total-seats", seats);
@@ -44,6 +71,13 @@ for (const btn of allBtn){
 //     setInnerText(id, sum);
 // }
 
+//alert section
 function setInnerText(id,value){
     document.getElementById(id).innerText = value;
 }
+
+// function grandTotalCost(){
+//     const totalPrice= document.getElementById('total-price').innerText
+//     const convertedTotalPrice= parseInt(totalPrice);
+//     setInnerText("grand-total",convertedTotalPrice);
+// }
